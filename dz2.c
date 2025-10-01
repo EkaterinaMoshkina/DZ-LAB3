1016c43e6a13716ec28ea88ec5b2d022ef0f3423
Мошкина Екатерина Юрьевна <moshkinakatya.240806@gmail.com>
2025-10-01 08:50:31 +0300

    dz2.c

--- dz2.c
+++ dz2.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
-#include <locale.h>
-#define D 63241.1
-
-int main()
-{
-	setlocale(LC_CTYPE, "RUS");
-	int g;
-	float as;
-	puts("введите число световых лет");
-	scanf("%d", &g);
-	printf("это в астрономических единицах %.2f", g * D);
-}
+##РґРѕР±Р°РІР»РµРЅ РЅРµ С‚РѕС‚ С„Р°Р№Р»
